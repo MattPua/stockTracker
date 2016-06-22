57276a2d66fb259a8290278cfd9785e3465d7d52
Matt Pua <mattpua49@gmail.com>
2016-06-21 23:48:26 -0400

    fixed hot module replacement

--- server.babel.js
+++ server.babel.js
@@ -7,9 +7,27 @@ import webpackConfig from './webpack.config.js';
 import bodyParser from 'body-parser';
 import MongoClient from 'mongodb';
 import DBConfig from './_config';
+import request from 'request';
 // Note: Need to use the babel file otherwise cannot use es6 style
 const app = express();
 const compiler = webpack(webpackConfig);
+
+// Extracts data from <form> elements and adds to body property in req object
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/src'));
+// TODO: Development Mode Only
+app.use(webpackMiddleware(compiler,{
+  stats: {
+    colors: true,
+    chunks: false,
+    'errors-only': true
+  },
+  publicPath: webpackConfig.output.publicPath
+}));
+app.use(webpackHotMiddleware(compiler,{
+  log: console.log
+}));
+
 
 let db;
 MongoClient.connect('mongodb://'+DBConfig.MONGO_USERNAME+':'+DBConfig.MONGO_PASSWORD+DBConfig.MONGO_APP,(err,database) =>{
@@ -23,19 +41,7 @@ MongoClient.connect('mongodb://'+DBConfig.MONGO_USERNAME+':'+DBConfig.MONGO_PASS
   });
 });
 
-// Extracts data from <form> elements and adds to body property in req object
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(__dirname + '/src'));
-app.use(webpackMiddleware(compiler,{
-  stats: {
-    colors: true,
-    chunks: false,
-    'errors-only': true
-  }
-}));
-app.use(webpackHotMiddleware(compiler,{
-  log: console.log
-}));
+
 app.get('/', (req, res) => {
   // Note: __dirname is the path to your current working directory. 
   res.sendFile(__dirname + '/index.html')
@@ -45,12 +51,20 @@ app.get('/', (req, res) => {
 });
 
 app.post('/quotes',(req,res) => {
-  console.log(req.body);
-  db.collection('quotes').save(req.body, (err,results) => {
+  request('http://finance.yahoo.com/d/quotes.csv?s='+req.body.stock+'.TO&f=nab', (error,response,body) =>{
+    console.log(response.statusCode);
+    if (!error && response.statusCode == 200) {
+            console.log(body) // Print the google web page.
+         }
+         else console.log(error);
+  });
+/*  db.collection('quotes').save(req.body, (err,results) => {
     if (err) return console.error(err);
     console.log("Saved to database");
     res.redirect('/');
-  });
+  });*/
+    res.redirect('/');
+
 });
 
 

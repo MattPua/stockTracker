f32ca5c3722ebca6978a2d21329823ddc100b44b
Matt Pua <mattpua49@gmail.com>
2016-06-29 13:16:05 -0400

    cleaned up the stockslist file so there's a sortBy for mobile too

--- src/app/Components/StocksList.js
+++ src/app/Components/StocksList.js
@@ -1,8 +1,21 @@
 import StockListItem from './StockListItem';
+import Helper from '../other/apphelper';
 import './StocksList.scss';
 class StocksList extends React.Component{
   constructor(props){
     super(props);
+  }
+  componentDidMount(){
+    $(this.refs.dropdown).dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: true, // Displays dropdown below the button
+        alignment: 'left' // Displays dropdown with edge aligned to the left of button
+      }
+    );
   }
   getStockListItems(type=''){
     let stockListItems = [];
@@ -20,6 +33,7 @@ class StocksList extends React.Component{
     return stockListItems;
   }
   onClick(event){
+    console.log('test');
     let prop = event.target.value;
     let direction = this.props.sortDirection;
     if (prop != this.props.sortBy)
@@ -30,6 +44,48 @@ class StocksList extends React.Component{
     this.props.changeSortBy(prop,direction);
   }
 
+  getPropertyNames(isForMobile){
+    let listItems = [];
+    let properties = ['name','symbol','price','targetPrice','sharesOwned','ask','bid','change'];
+    for (let item of properties){
+      if (isForMobile)
+        listItems.push(
+          <li>
+            <a href="#!" onClick={this.onClick.bind(this)} value={item}>{Helper.toUpperOne(item)} 
+              <i className="material-icons"  >swap_vert</i>
+            </a>
+          </li>
+        );
+      else
+        listItems.push(
+          <th data-field={item}>{Helper.toUpperOne(item)}<i value={item} className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
+        );
+    }
+    if (isForMobile)
+      return(
+        <ul id='dropdown' className='dropdown-content'>
+          {listItems}
+        </ul>
+      );
+    else
+      return[listItems];
+  }
+
+/*  getTableHeaders(){
+    return(
+      <th data-field='name'>Name<i value='name' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
+      <th data-field='symbol'>Symbol<i value='symbol' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
+      <th data-field='price'>Price<i value='price' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
+      <th data-field='targetPrice'>Target Price<i value='targetPrice' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
+      <th data-field='sharesOwned'>Shares Owned<i value='sharesOwned' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
+      <th data-field='ask'>Ask<i value='ask' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
+      <th data-field='bid'>Bid<i value='bid' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
+      <th data-field='change'>Change<i value='change' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
+      <th data-field='dayRange'>Day Range</th>
+      <th data-field='actions'>Actions</th>
+    );
+  }*/
+
   render(){
     return(
       <div className="stocks-list-container row">
@@ -37,14 +93,7 @@ class StocksList extends React.Component{
           <table className={"bordered highlight responsive-table"}>
             <thead>
               <tr>
-                <th data-field='name'>Name<i value='name' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
-                <th data-field='symbol'>Symbol<i value='symbol' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
-                <th data-field='price'>Price<i value='price' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
-                <th data-field='targetPrice'>Target Price<i value='targetPrice' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
-                <th data-field='sharesOwned'>Shares Owned<i value='sharesOwned' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
-                <th data-field='ask'>Ask<i value='ask' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
-                <th data-field='bid'>Bid<i value='bid' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
-                <th data-field='change'>Change<i value='change' className='material-icons' onClick={this.onClick.bind(this)}>swap_vert</i></th>
+                {this.getPropertyNames()}
                 <th data-field='dayRange'>Day Range</th>
                 <th data-field='actions'>Actions</th>
               </tr>
@@ -56,6 +105,8 @@ class StocksList extends React.Component{
         </div>
         <div className={'stocks-list hide-on-med-and-up ' + this.props.className}>
           <div className='row'>
+            <a className='dropdown-button btn' href='#' data-activates='dropdown' ref='dropdown'>Sort By:</a>
+            {this.getPropertyNames(true)}
             {this.getStockListItems('mobile')}
           </div>
         </div>

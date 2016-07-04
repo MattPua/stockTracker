cc8dc072acfbb43d422e8a8afae3e5e4e1a08440
Matt Pua <mattpua49@gmail.com>
2016-07-04 16:28:29 -0400

    started writing the readme, and added in the brcypt for passwords

---
+++ README.md
@@ -0,0 +1,30 @@
+=Portfolio Tracker=
+
+== Usage
+Can be tested using a guest login.
+username: guest
+password: guest
+
+== Features
+* Searching Stocks by Symbol Name through Yahoo Stocks API
+* Saving preferred stocks in MongoDB 
+* Editing stocks owned and shares owned of a particular stock to see gains/losses
+* Mobile Friendly Usage
+* Basic Authentication System using bcrypt
+
+== Technology
+* React
+* MaterializeCSS
+* Express Server
+* Node
+* MongoDB
+* Webpack
+* Yahoo Stocks API
+* REST API
+
+== Yahoo Stocks API
+Reference: http://www.jarloo.com/yahoo_finance/
+
+== TODO 
+
+* Put a limit on how fast you can refresh
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,7 @@ import * as Functions from '../functions';
 import _ from 'underscore';
 import MongoClient from 'mongodb';
 import request from 'request';
-
+import bcrypt from 'bcrypt-nodejs';
 module.exports=function(app,db){
 
   //Login
@@ -14,14 +14,21 @@ module.exports=function(app,db){
 
     // Password needs to be hashed and salted
     let password = req.body.password;
+
     db.collection('users').findOne({
       username: user,
-      password: password,
     },function(err,results){
       console.log(results);
       if (err)res.json({success: false,error: err.errMsg});
-      else if (results == null || results.length == 0) res.json({success: false,error: 'Username/password combination was incorrect'});
-      else res.cookie('userId',results._id,{ maxAge: 900000, httpOnly: true}).send({success: true, username: user}); 
+      if (results == null || results.length == 0) res.json({success: false, error:'No account with that username'});
+      // Now check the password hashes against each other
+      else{
+        bcrypt.compare(password,results.password,function(error,result){
+          if (error) console.error(error);
+          if (!result)res.json({success: false,error: 'Username/password combination was incorrect'});
+          else res.cookie('userId',results._id,{ maxAge: 900000, httpOnly: true}).send({success: true, username: user}); 
+        });
+      }
     });
   });
 
@@ -33,20 +40,23 @@ module.exports=function(app,db){
 
     // Password needs to be hashed and salted
     let password = req.body.password;
-    db.collection('users').save({
-      username: user,
-      password: password,
-    },function(err,results){
-      console.log(results);
-      if (err) {
-        let error = err.errMsg;
-        if (err.code == 11000) error = 'Username has already been taken';
-        res.json({success: false, error: error});
-      }
-      else {
-        res.json({success: true, username: user, _id: results._id});
-      }
-    });
+    bcrypt.hash(password,null,null,function(errHashing,hash){
+      if (errHashing) console.error(errHashing);
 
+      db.collection('users').save({
+        username: user,
+        password: hash,
+      },function(err,results){
+        console.log(results);
+        if (err) {
+          let error = err.errMsg;
+          if (err.code == 11000) error = 'Username has already been taken';
+          res.json({success: false, error: error});
+        }
+        else {
+          res.cookie('userId',results._id,{ maxAge: 900000, httpOnly: true}).send({success: true, username: user})
+        }
+      });
+    })
   });
 }

10b58f8f27f736bbdfaab25209e800896f3a8b7f
Matt Pua <mattpua49@gmail.com>
2016-07-04 15:39:20 -0400

    added in basic form validation for signup and login

--- src/app/Components/Login.js
+++ src/app/Components/Login.js
@@ -1,16 +1,44 @@
+import _ from 'underscore';
 export default class Login extends React.Component{
   constructor(props){
     super(props);
     this.state = {
       username: 'guest',
       password: 'guest',
+      errors: [],
+      errorMessages:{
+        username: 'wrong',
+        password: 'wrong',
+      }
     };
+  }
+
+  isFormValid(){
+    let errors = [];
+    let errorMessages = _.extend({},this.state.errorMessages);
+    if (this.state.username == '' || this.state.username == null)
+      errors.push({key: 'username',error: 'Username cannot be empty!'});
+    if (this.state.password == '' || this.state.password == null)
+      errors.push({key: 'password',error:'Password cannot be empty!'});
+
+
+    if (errors.length){
+      let i = 0;
+      for (let e of errors){
+        if (i++ == 0) $(this.refs[e.key]).focus();
+        $(this.refs[e.key]).addClass('invalid');
+        errorMessages[e.key] = e.error;
+      }
+    }
+
+    this.setState({errors: errors,errorMessages: errorMessages});
+    if (errors.length) return false;
+    return true;
   }
 
   signup(event){
     event.preventDefault();
-    // TODO: Should display errors
-    if (this.state.username == '' || this.state.password == '') return;
+    if (!this.isFormValid()) return;
     this.props.signup({
       username: this.state.username,
       password: this.state.password,
@@ -19,6 +47,7 @@ export default class Login extends React.Component{
 
   login(event){
     event.preventDefault();
+    if (!this.isFormValid()) return;
     this.props.login({
       username: this.state.username,
       password: this.state.password,
@@ -44,24 +73,24 @@ export default class Login extends React.Component{
       <div className='login-container'>
         <div className='login row'>
           <form className="col s12" >
-            <h3 className='center'>Login Page</h3>
+            <h3 className='center'>Login</h3>
             <div className="row">
               <div className="input-field col s12">
-                <input id="username" type="text" className="validate" value={this.state.username} onChange={this.onUserChange.bind(this)}/>
-                <label for="username">Username</label>
+                <input id="username" type="text" className="validate" value={this.state.username} ref='username' onChange={this.onUserChange.bind(this)}/>
+                <label for="username" data-error={this.state.errorMessages.username}>Username</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input id="password" type="password" className="validate" value={this.state.password} onChange={this.onPasswordChange.bind(this)}/>
-                <label for="password">Password</label>
+                <input id="password" type="password" className="validate" value={this.state.password} ref='password' onChange={this.onPasswordChange.bind(this)}/>
+                <label for="password" data-error={this.state.errorMessages.password}>Password</label>
               </div>
             </div>
             <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.login.bind(this)}>Login
               <i className="material-icons right">send</i>
             </button>
             <button className="btn waves-effect waves-light" type="submit" name="action" onClick={this.signup.bind(this)}>Sign up
-              <i className="material-icons right">send</i>
+              <i className="material-icons right">perm_identity</i>
             </button>
           </form>
         </div>

2feec4fb258bbfa48ee354c200412993fb478a22
Matt Pua <mattpua49@gmail.com>
2016-07-05 12:48:45 -0400

    more colour specification

--- src/app/Components/FixedItems.js
+++ src/app/Components/FixedItems.js
@@ -2,13 +2,16 @@ export default class FixedItems extends React.Component{
   constructor(props){
     super(props);
   }
+  componentDidMount(){
+    $(this.refs.refresh).tooltip();
+  }
 
   render(){
     return(
       <div className='fixed-items-container'>
         <div className="fixed-action-btn" >
           <a className="btn-floating btn waves-effect waves-light" onClick={this.props.refreshList}>
-            <i className='material-icons tooltipped' data-position='top' data-delay='50' data-tooltip='Refresh'>refresh</i>
+            <i className='material-icons tooltipped' data-position='top' data-delay='50' data-tooltip='Refresh' ref='refresh'>refresh</i>
           </a>
         </div>
 {/*        <div className="fixed-action-btn">

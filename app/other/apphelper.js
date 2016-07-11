6da99f60670c17175e29277b5124519212beb8dd
Matt Pua <mattpua49@gmail.com>
2016-07-11 12:59:01 -0400

    Updates

--- app/other/apphelper.js
+++ app/other/apphelper.js
@@ -37,7 +37,7 @@ class AppHelper{
   }
 
   static ajaxConfig(url,type,data){
-    let base = 'http://159.203.3.185:3000/';
+    let base = 'http://159.203.42.58:3000/api/';
     return {
       url: base + url,
       type: type,
